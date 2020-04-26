9bf1b7885a11bebff91b28ec2cc6d5c0436abfab
Maurice <maurice@rlg.local>
2020-04-25 22:14:39 -0500

    Add controller method edit and test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,36 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
   end
 
-  def edit
+  def create
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+  end
+
+
   def delete
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    # Form Authentication
+    params.require(:user)
+    # Regulate mass assignment
+    .permit(:first_name, :last_name, :email)
+    # 1.Instantiate a new user using params from new form
   end
 end
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,24 +1,36 @@
 require 'rails_helper'
 
 describe UsersController, type: :request do
+
   let(:users) { User.all}
   describe "GET /index" do
     it "returns http success" do
-      get "/users/index"
+      get "/"
       expect(response).to have_http_status(:success)
     end
     it 'assigns all users to @users' do
-      get "/users/index"
+      get "/"
       expect(assigns('users')).to eq(users)
     end
   end
   
-  # describe "GET /show" do
-  #   it "returns http success" do
-  #     get "/users/show"
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET /show" do
+  let(:user) { User.find(params[:id]) }
+    before (:example) do
+      def user
+        @user ||= User.new
+       end
+      end
+    it "returns http success" do
+      get "/users/"
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'Finds a user by id' do
+      get "/users/"
+      expect(assigns('user')).to eq(user.id)
+    end
+  end
 
   # describe "GET /new" do
   #   it "returns http success" do
@@ -26,13 +38,22 @@ describe UsersController, type: :request do
   #     expect(response).to have_http_status(:success)
   #   end
   # end
-
-  # describe "GET /edit" do
-  #   it "returns http success" do
-  #     get "/users/edit"
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET /edit" do
+    let(:user) { User.edit(params[:id]) }
+    before (:example) do
+      def user
+        @user ||= User.new
+       end
+      end
+    it "returns http success" do
+      get "/users/"
+      expect(response).to have_http_status(:success)
+    end
+    it 'Finds a user by id' do
+      get "/users/"
+      expect(assigns('user')).to eq(user.id)
+    end
+  end
 
   # describe "GET /delete" do
   #   it "returns http success" do
