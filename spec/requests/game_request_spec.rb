62cd02db3a2fae53c31225aa2ed8d2e6fa915343
Maurice <maurice@rlg.local>
2020-04-27 15:10:10 -0500

    Add game controller tests

--- spec/requests/game_request_spec.rb
+++ spec/requests/game_request_spec.rb
@@ -1,25 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe "Games", type: :request do
+describe GameController, type: :request do
 
   describe "GET /start" do
+    before(:example) { get "/game/start" } 
+
     it "returns http success" do
-      get "/game/start"
       expect(response).to have_http_status(:success)
+    end
+    it 'renders /start template' do
+      expect(response).to render_template('start')
     end
   end
 
   describe "GET /restart" do
+    before(:example) { get "/game/restart" }
+
     it "returns http success" do
-      get "/game/restart"
       expect(response).to have_http_status(:success)
+    end
+    it 'renders /restart template' do
+      expect(response).to render_template('restart')
     end
   end
 
   describe "GET /close" do
+    before(:example) { get "/game/close" }
+
     it "returns http success" do
-      get "/game/close"
       expect(response).to have_http_status(:success)
+    end
+    it 'renders /close template' do
+      expect(response).to render_template('close')
     end
   end
 
