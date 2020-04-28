d8dada7d58e9c76afa99201fc340a068d0ac5476
Maurice <maurice@rlg.local>
2020-04-28 16:45:03 -0500

    Add game object

---
+++ app/javascript/packs/game.js
@@ -0,0 +1,17 @@
+class Gameobject {
+    constructor () {
+        this. X_Storage = []
+        this.O_Storage = []
+    }
+    changeXorO () {
+        return "X or O"
+    }
+}
+
+// export default Gameobject 
+// document.appendChild("div", "hello")
+
+obj = new Gameobject()
+
+console.log(obj.changeXorO())
+console.log('You clicked =>', document.childNodes)
